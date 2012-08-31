94290db46685f936fa4f24124029c488cfaccfe3
zonyitoo <zonyitoo@gmail.com>
2012-08-31 13:32:19 +0800

    UI部分小修改

--- src/edu/sysuedaily/ui/VisualActivity.java
+++ src/edu/sysuedaily/ui/VisualActivity.java
@@ -1,15 +1,54 @@
 package edu.sysuedaily.ui;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+
+import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.MenuItem;
+import com.viewpagerindicator.CirclePageIndicator;
+
+import edu.sysuedaily.R;
+import edu.sysuedaily.utils.VisualPagerAdapter;
 
 public class VisualActivity extends SherlockFragmentActivity {
 
+	ViewPager pager;
+	CirclePageIndicator indicator;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_visual);
+		
+		pager = (ViewPager) findViewById(R.id.pager);
+		indicator = (CirclePageIndicator) findViewById(R.id.indicator);
+		
+		ArrayList<Map<String, Object>> arrayList = new ArrayList<Map<String,Object>>();
+		for (int i = 0; i < 10; ++ i) {
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			arrayList.add(map);
+		}
+		
+		pager.setAdapter(new VisualPagerAdapter(this, arrayList));
+		indicator.setViewPager(pager);
+		
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+		case android.R.id.home:
+			finish();
+			break;
+		}
+		
+		
+		return super.onOptionsItemSelected(item);
+	}
 }
---
+++ src/edu/sysuedaily/utils/VisualPagerAdapter.java
@@ -0,0 +1,56 @@
+package edu.sysuedaily.utils;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import edu.sysuedaily.R;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class VisualPagerAdapter extends PagerAdapter {
+
+	ArrayList<Map<String, Object>> arrayList;
+	Context context;
+	
+	public VisualPagerAdapter(Context context, ArrayList<Map<String, Object>> array) {
+		arrayList = array;
+		this.context = context;
+	}
+	
+	@Override
+	public int getCount() {
+		return arrayList.size();
+	}
+
+	@Override
+	public boolean isViewFromObject(View arg0, Object arg1) {
+
+		return arg0 == arg1;
+	}
+
+	@Override
+	public Object instantiateItem(ViewGroup container, int position) {
+		LayoutInflater inflater = LayoutInflater.from(context);
+		View view = inflater.inflate(R.layout.pagercontent_activity_visual, null);
+		ImageView imageView = (ImageView) view.findViewById(R.id.image);
+		TextView textView = (TextView) view.findViewById(R.id.text);
+		
+		((ViewPager) container).addView(view);
+		return view;
+	}
+
+	@Override
+	public void destroyItem(ViewGroup container, int position, Object object) {
+		((ViewPager) container).removeView((View) object);
+	}
+	
+	
+
+}
